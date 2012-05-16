717f283064702277cd0eaa93cacd3696e45db5af
Kazuya Takeshima <mail@mitukiii.jp>
2012-05-17 00:19:07 +0900

    局所関数を無名関数内に移動/整理

--- src/javascripts/popup.js
+++ src/javascripts/popup.js
@@ -3,42 +3,44 @@
   var MakeBooth = backgroundPage.MakeBooth;
   var timeline = null;
 
-  var paddingLeft = function(input, length, string) {
-    input = input.toString();
-    string = string.toString();
-    while (input.length < length) {
-      input = string + input;
-    }
+  var createStatus = (function() {
+    var paddingLeft = function(input, length, string) {
+      input = input.toString();
+      string = string.toString();
+      while (input.length < length) {
+        input = string + input;
+      }
 
-    return input.substring(input.length - length, length);
-  };
+      return input.substring(input.length - length, length);
+    };
 
-  var formatDate = function(date) {
-    return date.getFullYear() + '/' +
-      paddingLeft(date.getMonth(), 2, '0') + '/' +
-      paddingLeft(date.getDate(), 2, '0') + ' ' +
-      paddingLeft(date.getHours(), 2, '0') + ':' +
-      paddingLeft(date.getMinutes(), 2, '0');
-  };
+    var formatDate = function(date) {
+      return date.getFullYear() + '/' +
+        paddingLeft(date.getMonth(), 2, '0') + '/' +
+        paddingLeft(date.getDate(), 2, '0') + ' ' +
+        paddingLeft(date.getHours(), 2, '0') + ':' +
+        paddingLeft(date.getMinutes(), 2, '0');
+    };
 
-  var createStatus = function(datum) {
-    var status = document.createElement('li');
-    status.className = 'status';
-    status.innerHTML = [
-      '<div class="column-left">',
-      '  <div class="user-image">',
-      '    <img src="' + datum.user_image_file_name + '" />',
-      '  </div>',
-      '</div>',
-      '<div class="column-right">',
-      '  <div class="event ' + datum.event_class + '"></div>',
-      '  <div class="text">' + datum.text + '</div>',
-      '  <div class="created_at">' + formatDate(datum.created_at) + '</div>',
-      '</div>'
-    ].join("\n");
+    return function(datum) {
+      var status = document.createElement('li');
+      status.className = 'status';
+      status.innerHTML = [
+        '<div class="column-left">',
+        '  <div class="user-image">',
+        '    <img src="' + datum.user_image_file_name + '" />',
+        '  </div>',
+        '</div>',
+        '<div class="column-right">',
+        '  <div class="event ' + datum.event_class + '"></div>',
+        '  <div class="text">' + datum.text + '</div>',
+        '  <div class="created_at">' + formatDate(datum.created_at) + '</div>',
+        '</div>'
+      ].join("\n");
 
-    return status;
-  };
+      return status;
+    };
+  }());
 
   var addStatusToTimeline = function(datum) {
     if (! timeline) {
