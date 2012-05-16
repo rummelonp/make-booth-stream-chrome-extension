f8478407e227f33b023e5a3d4d2270cba1a2a895
Kazuya Takeshima <mail@mitukiii.jp>
2012-05-16 16:46:00 +0900

    ステータスがない場合はバッヂを表示しない

--- src/javascripts/background.js
+++ src/javascripts/background.js
@@ -4,20 +4,24 @@
 
   var action = chrome.browserAction;
 
-  MakeBooth.observe('open', function() {
-    action.setBadgeBackgroundColor({color: GREEN});
-    action.setBadgeText({text: MakeBooth.getData().length.toString()});
-  });
+  var toggleBadge = function() {
+    if (MakeBooth.hasConnection()) {
+      var count = MakeBooth.getData().length;
+      action.setBadgeBackgroundColor({color: GREEN});
+      if (count > 0) {
+        action.setBadgeText({text: count.toString()});
+      } else {
+        action.setBadgeText({text: ''});
+      }
+    } else {
+      action.setBadgeBackgroundColor({color: RED});
+      action.setBadgeText({text: '!'});
+    }
+  };
 
-  MakeBooth.observe('message', function() {
-    action.setBadgeBackgroundColor({color: GREEN});
-    action.setBadgeText({text: MakeBooth.getData().length.toString()});
-  });
-
-  MakeBooth.observe('close', function() {
-    action.setBadgeBackgroundColor({color: RED});
-    action.setBadgeText({text: '!'});
-  });
+  MakeBooth.observe('open', toggleBadge);
+  MakeBooth.observe('message', toggleBadge);
+  MakeBooth.observe('close', toggleBadge);
 
   MakeBooth.connect();
 }());
